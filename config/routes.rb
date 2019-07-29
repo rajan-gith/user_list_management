1dd2a58063843df47413894dfc22545eb2a008bd
rajan-gith <rajan.prajapati@berylsystems.com>
2019-07-29 15:09:40 +0530

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
+  get 'excel/users', to: 'users#excel', as: 'users_excel'
 end
